13bf766b3ba529c24750d228e48613ae6b33cfc3
Michael Brennan <brennan.brisad@gmail.com>
2014-09-16 17:52:50 +0200

    Refactor tests into classes

--- tests/test_grec.py
+++ tests/test_grec.py
@@ -11,85 +11,98 @@ Tests for `grec` module.
 import pytest
 import grec
 
-def test_literal():
-    m = grec.Matcher()
-    m.add_pattern('x', 'blue')
-    assert str(m.match('axbxc')) == 'a\x1b[34mx\x1b[0mb\x1b[34mx\x1b[0mc'
 
-def test_literal_with_background():
-    m = grec.Matcher()
-    m.add_pattern('x', 'blue', 'on_white')
-    assert str(m.match('axbxc')) == 'a\x1b[47m\x1b[34mx\x1b[0mb\x1b[47m\x1b[34mx\x1b[0mc'
+class TestMatcher(object):
 
-def test_wildcard():
-    m = grec.Matcher()
-    m.add_pattern('x.x', 'red')
-    assert str(m.match('axbxc')) == 'a\x1b[31mxbx\x1b[0mc'
+    @pytest.fixture
+    def matcher(self):
+        return grec.Matcher()
 
-def test_add_pattern_same_replaces():
-    m = grec.Matcher()
-    m.add_pattern('axbxc', 'red')
-    m.add_pattern('b', 'blue')
-    m.add_pattern('axbxc', 'green')
-    assert str(m.match('axbxc')) == '\x1b[32maxbxc\x1b[0m'
-    assert len(m.patterns) == 2
+    def check(self, matcher, result_string):
+        assert str(matcher.match('axbxc')) == result_string
 
-def test_remove_pattern():
-    m = grec.Matcher()
-    m.add_pattern('x', 'red')
-    m.remove_pattern('x')
-    assert str(m.match('axbxc')) == 'axbxc'
+    def test_literal(self, matcher):
+        matcher.add_pattern('x', 'blue')
+        self.check(matcher, 'a\x1b[34mx\x1b[0mb\x1b[34mx\x1b[0mc')
 
-def test_multiple_patterns():
-    m = grec.Matcher()
-    m.add_pattern('a', 'red')
-    m.add_pattern('b', 'green')
-    m.add_pattern('c', 'blue')
-    assert str(m.match('axbxc')) == '\x1b[31ma\x1b[0mx\x1b[32mb\x1b[0mx\x1b[34mc\x1b[0m'
+    def test_literal_with_background(self, matcher):
+        matcher.add_pattern('x', 'blue', 'on_white')
+        self.check(matcher,
+                   'a\x1b[47m\x1b[34mx\x1b[0mb\x1b[47m\x1b[34mx\x1b[0mc')
 
-def test_overlapping_patterns():
-    m = grec.Matcher()
-    m.add_pattern('ax', 'red')
-    m.add_pattern('xc', 'green')
-    m.add_pattern('xbx', 'blue')
-    assert str(m.match('axbxc')) == '\x1b[31ma\x1b[0m\x1b[34mxbx\x1b[0m\x1b[32mc\x1b[0m'
-    m = grec.Matcher()
-    m.add_pattern('axbxc', 'blue')
-    m.add_pattern('xbx', 'red')
-    assert str(m.match('axbxc')) == '\x1b[34ma\x1b[0m\x1b[31mxbx\x1b[0m\x1b[34mc\x1b[0m'
-    m = grec.Matcher()
-    m.add_pattern('axb', 'green')
-    m.add_pattern('xb', 'blue')
-    assert str(m.match('axbxc')) == '\x1b[32ma\x1b[0m\x1b[34mxb\x1b[0mxc'
-    m = grec.Matcher()
-    m.add_pattern('axb', 'green')
-    m.add_pattern('ax', 'blue')
-    assert str(m.match('axbxc')) == '\x1b[34max\x1b[0m\x1b[32mb\x1b[0mxc'
+    def test_wildcard(self, matcher):
+        matcher.add_pattern('x.x', 'red')
+        self.check(matcher, 'a\x1b[31mxbx\x1b[0mc')
 
-def test_intervals_overlap():
-    intervals = grec.grec.Intervals({(1, 5): None, (6, 8): None})
-    assert intervals.overlap((0, 1)) == set()
-    assert intervals.overlap((1, 2)) == set([(1, 5)])
-    assert intervals.overlap((1, 6)) == set([(1, 5)])
-    assert intervals.overlap((5, 6)) == set()
-    assert intervals.overlap((4, 10)) == set([(1, 5), (6, 8)])
-    assert intervals.overlap((5, 10)) == set([(6, 8)])
-    assert intervals.overlap((10, 10)) == set()
+    def test_add_pattern_same_replaces(self, matcher):
+        matcher.add_pattern('axbxc', 'red')
+        matcher.add_pattern('b', 'blue')
+        matcher.add_pattern('axbxc', 'green')
+        self.check(matcher, '\x1b[32maxbxc\x1b[0m')
+        assert len(matcher.patterns) == 2
 
-def test_intervals_mutable_mapping():
-    intervals = grec.grec.Intervals()
-    intervals[(5, 10)] = 'abc'
-    assert intervals[(5, 10)] == 'abc'
-    assert intervals == {(5, 10): 'abc'}
-    intervals[(1, 2)] = 'def'
-    assert intervals.keys() == [(1, 2), (5, 10)]  # Sorted order
-    assert len(intervals) == 2
-    del intervals[(1, 2)]
-    assert len(intervals) == 1
+    def test_remove_pattern(self, matcher):
+        matcher.add_pattern('x', 'red')
+        matcher.remove_pattern('x')
+        self.check(matcher, 'axbxc')
 
-def test_bad_interval():
-    intervals = grec.grec.Intervals()
-    with pytest.raises(AssertionError):
-        intervals[(0, 0)] = 'ghi'
-    with pytest.raises(AssertionError):
-        intervals = grec.grec.Intervals({(1, 1): None})
+    def test_multiple_patterns(self, matcher):
+        matcher.add_pattern('a', 'red')
+        matcher.add_pattern('b', 'green')
+        matcher.add_pattern('c', 'blue')
+        self.check(matcher,
+                   '\x1b[31ma\x1b[0mx\x1b[32mb\x1b[0mx\x1b[34mc\x1b[0m')
+
+    def test_partial_overlap(self, matcher):
+        matcher.add_pattern('ax', 'red')
+        matcher.add_pattern('xc', 'green')
+        matcher.add_pattern('xbx', 'blue')
+        self.check(matcher,
+                   '\x1b[31ma\x1b[0m\x1b[34mxbx\x1b[0m\x1b[32mc\x1b[0m')
+
+    def test_overlap_new_interval_within(self, matcher):
+        matcher.add_pattern('axbxc', 'blue')
+        matcher.add_pattern('xbx', 'red')
+        self.check(matcher,
+                   '\x1b[34ma\x1b[0m\x1b[31mxbx\x1b[0m\x1b[34mc\x1b[0m')
+
+    def test_overlap_right_aligned(self, matcher):
+        matcher.add_pattern('axb', 'green')
+        matcher.add_pattern('xb', 'blue')
+        self.check(matcher, '\x1b[32ma\x1b[0m\x1b[34mxb\x1b[0mxc')
+
+    def test_overlap_left_aligned(self, matcher):
+        matcher.add_pattern('axb', 'green')
+        matcher.add_pattern('ax', 'blue')
+        self.check(matcher, '\x1b[34max\x1b[0m\x1b[32mb\x1b[0mxc')
+
+
+class TestIntervals(object):
+
+    def test_intervals_overlap(self):
+        intervals = grec.grec.Intervals({(1, 5): None, (6, 8): None})
+        assert intervals.overlap((0, 1)) == set()
+        assert intervals.overlap((1, 2)) == set([(1, 5)])
+        assert intervals.overlap((1, 6)) == set([(1, 5)])
+        assert intervals.overlap((5, 6)) == set()
+        assert intervals.overlap((4, 10)) == set([(1, 5), (6, 8)])
+        assert intervals.overlap((5, 10)) == set([(6, 8)])
+        assert intervals.overlap((10, 10)) == set()
+
+    def test_intervals_mutable_mapping(self):
+        intervals = grec.grec.Intervals()
+        intervals[(5, 10)] = 'abc'
+        assert intervals[(5, 10)] == 'abc'
+        assert intervals == {(5, 10): 'abc'}
+        intervals[(1, 2)] = 'def'
+        assert intervals.keys() == [(1, 2), (5, 10)]  # Sorted order
+        assert len(intervals) == 2
+        del intervals[(1, 2)]
+        assert len(intervals) == 1
+
+    def test_bad_interval(self):
+        intervals = grec.grec.Intervals()
+        with pytest.raises(AssertionError):
+            intervals[(0, 0)] = 'abc'
+        with pytest.raises(AssertionError):
+            intervals = grec.grec.Intervals({(1, 1): None})
